290cfa0f1dc8a06ec7a8c75f618e538dd7a9481a
Muthu Sankar <reachtoshankarsk@gmail.com>
2022-03-13 11:16:03 +0530

    equipement-hiring

---
+++ build/precache-manifest.7ba8fab92847a1ae04535635309238f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6028d2f2d80fad0dc19e2ec53248c1b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e9a0da90fcb1b7a961",
+    "url": "/static/css/main.30fa3287.chunk.css"
+  },
+  {
+    "revision": "9fecaf8c904c3e38bed5",
+    "url": "/static/js/2.83d781a2.chunk.js"
+  },
+  {
+    "revision": "fdb6530d516471fe9b21cbdc14e06649",
+    "url": "/static/js/2.83d781a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e9a0da90fcb1b7a961",
+    "url": "/static/js/main.23fb9cb7.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "59351ca5ee6c42ea3c6840c9a6c8b37e",
+    "url": "/static/media/default-image-background.59351ca5.jpeg"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
---
+++ src/components/Nav/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+import Alamo from "../AlamoIcon";
+
+function Nav() {
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">Order History</Link>
+          </li>
+          <li className="mx-1">
+            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+          <ul className="flex-row">
+            <li>
+              <Link to="/signup" className="signup-login">
+                Signup
+              </Link>
+            </li>
+            <li className="mx-4">
+              <Link to="/login" className="signup-login">
+                Login
+              </Link>
+            </li>
+          </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <Alamo/>
+      <h1 className="px-3 home-banner-font">
+        <Link to="/">ALAMO TOOLS</Link>
+      </h1>
+
+      <nav className="nav-links">{showNavigation()}</nav>
+    </header>
+  );
+}
+
+export default Nav;
